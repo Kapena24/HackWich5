76170f54bafd23f261a43521e8e402912b5473cc
Kapena Kaaihue <142773521+Kapena24@users.noreply.github.com>
2023-10-04 09:16:40 -1000

    Completed Parts 1-7

--- HackWich_5/ViewController.swift
+++ HackWich_5/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topLabel: UILabel!
+
+    
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+    var currentIndex: Int = 0
+    
+var favoriteFoodsArray: [String] = ["Pizza", "Sushi", "Burgers", "Ice Cream", "Tacos"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+if let favoriteFood = favoriteFoodsArray.first {
+bottomLabel.text = favoriteFoodsArray[3]
+} else {
 
+}
+    }
+    
 }
 
