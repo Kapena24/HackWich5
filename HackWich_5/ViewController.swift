b7af6c303c02e515f9b615039d35375c30aeba71
Kapena Kaaihue <142773521+Kapena24@users.noreply.github.com>
2023-10-04 10:00:32 -1000

    Completed Part 1 of 2 parts

--- HackWich_5/ViewController.swift
+++ HackWich_5/ViewController.swift
@@ -9,26 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var topLabel: UILabel!
+@IBOutlet weak var topLabel: UILabel!
 
+@IBOutlet weak var favoriteMovieLable: UILabel!
+
+
+@IBOutlet weak var secondTopLabel: UILabel!
     
-    @IBOutlet weak var bottomLabel: UILabel!
+@IBOutlet weak var bottomLabel: UILabel!
     
     var currentIndex: Int = 0
     
 var favoriteFoodsArray: [String] = ["Pizza", "Sushi", "Burgers", "Ice Cream", "Tacos"]
+
+    var favoriteMovieArry: [String] = ["Avatar", "Spiderman", "ZombieLand", "Ip Man"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func buttonPressed(_ sender: UIButton) {
+@IBAction func buttonPressed(_ sender: UIButton) {
+
 if let favoriteFood = favoriteFoodsArray.first {
+        
 bottomLabel.text = favoriteFoodsArray[3]
-} else {
+        
+    } else {
 
 }
     }
     
+//Movies\\
+    
+    @IBAction func showButtonPressed(_ sender: UIButton) {
+if let favoriteMovie = favoriteMovieArry.first {
+
+favoriteMovieLable.text = favoriteMovieArry[0]
+            
+} else {
+            
+    }
+        
+    }
 }
 
